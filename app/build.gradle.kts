baf68c80927730a42767bfa662d55c618eef0400
Shiina Sugioka <shiina12@uw.edu>
2023-12-04 19:24:52 -0800

    set up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,21 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.api-client:google-api-client-android:1.23.0")
-    implementation("com.google.apis:google-api-services-tasks:v1-rev20230401-2.0.0"){
-        exclude("org.apache.httpcomponents")
-    }
-
-    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+//    implementation("com.google.android.gms:play-services-auth:20.7.0")
+//    implementation("com.google.api-client:google-api-client-android:1.23.0")
+//    implementation("com.google.apis:google-api-services-tasks:v1-rev20230401-2.0.0"){
+//        exclude("org.apache.httpcomponents")
+//    }
+//
+//    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+//
+//    implementation("androidx.core:core-ktx:1.12.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.10.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -52,4 +60,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation("com.google.apis:google-api-services-calendar:v3-rev305-1.23.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+//    implementation("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava")
+
+    implementation("com.google.api-client:google-api-client-android:1.23.0") {
+        exclude("org.apache.httpcomponents")
+    }
+
+    implementation("pub.devrel:easypermissions:3.0.0")
 }
--- app/src/main/java/edu/uw/ischool/shiina12/tasknest_tester/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/shiina12/tasknest_tester/MainActivity.kt
@@ -2,94 +2,13 @@ package edu.uw.ischool.shiina12.tasknest_tester
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.JsonFactory
-import com.google.api.client.json.gson.GsonFactory
-import com.google.api.client.util.store.FileDataStoreFactory
-import com.google.api.services.tasks.Tasks
-import com.google.api.services.tasks.TasksScopes
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
-import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
-
-    private val APPLICATION_NAME = "Google Tasks API Java Quickstart"
-    private val JSON_FACTORY: JsonFactory = GsonFactory.getDefaultInstance()
-    private val TOKENS_DIRECTORY_PATH = "tokens"
-
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private val SCOPES = listOf(TasksScopes.TASKS_READONLY)
-//    private val file = File(resources.getResourceName())
-    private val CREDENTIALS_FILE_PATH = "credentials.json"
-
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    @Throws(IOException::class)
-    private fun getCredentials(HTTP_TRANSPORT: NetHttpTransport): Credential? {
-        // Load client secrets.
-        val inp =
-            MainActivity::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
-                ?: throw FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH)
-        val clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(inp))
-
-        // Build flow and trigger user authorization request.
-        val flow = GoogleAuthorizationCodeFlow.Builder(
-            HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES
-        )
-            .setDataStoreFactory(FileDataStoreFactory(File(TOKENS_DIRECTORY_PATH)))
-            .setAccessType("offline")
-            .build()
-        val receiver = LocalServerReceiver.Builder().setPort(8888).build()
-        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Build a new authorized API client service.
-        val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-        val service = Tasks.Builder(
-            HTTP_TRANSPORT,
-            JSON_FACTORY,
-            getCredentials(HTTP_TRANSPORT)
-        )
-            .setApplicationName(APPLICATION_NAME)
-            .build()
-
-        // Print the first 10 task lists.
-
-        // Print the first 10 task lists.
-        val result = service.tasklists().list()
-            .setMaxResults(10)
-            .execute()
-        val taskLists = result.items
-        if (taskLists == null || taskLists.isEmpty()) {
-            println("No task lists found.")
-        } else {
-            println("Task lists:")
-            for (tasklist in taskLists) {
-                System.out.printf("%s (%s)\n", tasklist.title, tasklist.id)
-            }
-        }
     }
-
 
 }
